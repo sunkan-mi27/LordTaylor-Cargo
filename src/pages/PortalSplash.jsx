ef544181c1752479f6127e556ced35b3a98a3888
sunkanmi <iolasunkanmi405@gmail.com>
2026-08-07 14:13:05 +0100

    PortalSplash stylesheet path

--- src/pages/PortalSplash.jsx
+++ src/pages/PortalSplash.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 import { FaCircleCheck, FaBoxesStacked, FaShieldHalved } from "react-icons/fa6";
 
-import "../styles/portalSplash.css";
+import "../styles/portalsplash.css";
 
 const PortalSplash = () => {
   const navigate = useNavigate();
