673c7b94cfe9e75509d676a4f6404267f7409152
sunkanmi <iolasunkanmi405@gmail.com>
2026-09-01 06:26:38 +0100

    Path Changed

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
 
       <Route path="/register" element={<Register />} />
 
-      <Route path="/admin" element={<AdminRegister />} />
+      <Route path="/admin-portal" element={<AdminRegister />} />
 
       <Route path="/forgot-password" element={<ForgotPassword />} />
 
